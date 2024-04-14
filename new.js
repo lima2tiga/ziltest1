a117d9084d1b1cb2205915ee9508a0aa5ccb9582
lima2tiga <131317112+lima2tiga@users.noreply.github.com>
2024-04-14 20:24:57 +0800

    Create new.js

---
+++ new.js
@@ -0,0 +1,21 @@
+function RedeemBox(hour, minute, second, millisecond) {
+    var now = new Date();
+    var targetTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hour, minute, second, millisecond);
+
+    var timeRemaining = targetTime.getTime() - now.getTime();
+
+    if (timeRemaining > 0) {
+        setTimeout(function() {
+            var element = document.querySelector('.stardust-toggle-toggle');
+            if (element) {
+                element.click();
+                var redeemTime = new Date(); // Get the current time when the action is actually executed
+                console.log("Redeem at : " + redeemTime.getHours() + ":" + redeemTime.getMinutes() + ":" + redeemTime.getSeconds() + ":" + redeemTime.getMilliseconds());
+            }
+        }, timeRemaining);
+    } else {
+        console.log("Target time has already passed.");
+    }
+}
+
+RedeemBox(15, 43, 0, 0);
